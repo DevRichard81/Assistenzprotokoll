4c59d7ed4f7af05ac9fd076218dd2801d10a5e99
DevelopeRichard <le8700@outlook.com>
2026-05-15 18:18:04 +0200

    fixes for github workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: './',
+  base: '/Assistenzprotokoll/',
   plugins: [
     react(),
     VitePWA({
@@ -17,24 +17,24 @@ export default defineConfig({
         background_color: '#f8fafc',
         display: 'standalone',
         orientation: 'portrait-primary',
-        start_url: '/',
-        scope: '/',
+        start_url: '/Assistenzprotokoll/',
+        scope: '/Assistenzprotokoll/',
         lang: 'de',
         icons: [
           {
-            src: '/pwa-icon.svg',
+            src: 'pwa-icon.svg',
             sizes: '192x192',
             type: 'image/svg+xml',
             purpose: 'any',
           },
           {
-            src: '/pwa-icon.svg',
+            src: 'pwa-icon.svg',
             sizes: '512x512',
             type: 'image/svg+xml',
             purpose: 'any',
           },
           {
-            src: '/mask-icon.svg',
+            src: 'mask-icon.svg',
             sizes: '512x512',
             type: 'image/svg+xml',
             purpose: 'maskable',
